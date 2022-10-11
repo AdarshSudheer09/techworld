08ef529b7bef4e3a289df47ade94bdd97d30bc23
AdarshSudheer09 <111100446+AdarshSudheer09@users.noreply.github.com>
2022-10-10 19:30:19 -0700

    console

--- docs/index.js
+++ docs/index.js
@@ -2,3 +2,9 @@
  console.log('Hello World');
  let name = 'Adarsh';
  console.log(name);
+
+ //cannot be a reserved keyword
+ //should be meaningful
+ //cannot start with a number
+ //cannot contain a space or a hyphen
+ //Are case-sensitive
